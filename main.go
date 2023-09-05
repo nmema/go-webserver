46a81a0b6a3ec4b83804f90d0657cab0b036df2b
nmema <nicolas.a.mema@gmail.com>
2023-09-05 20:58:19 -0300

    #9 move handlerReadiness

--- main.go
+++ main.go
@@ -29,9 +29,3 @@ func main() {
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
 }
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
